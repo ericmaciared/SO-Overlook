9c2ed33d640391f90cff61ded600c5b5f968ded6
eric.macia <eric.macia@montserrat.salle.url.edu>
2020-10-17 13:30:44 +0200

    Add config file processing

--- danny.c
+++ danny.c
@@ -37,6 +37,122 @@ typedef struct Data{
 }Data;
 
 //FUNCTIONS
+void processConfig(Data* data, const char* file){
+    char buffer[128];
+    char ptr;
+    int fd = -1;
+    int count = 0;
+
+    fd = open(file, O_RDONLY);
+    if (fd < 0) {
+      sprintf(buffer, "Error opening %s", file);
+      perror(buffer);
+      raise(SIGINT);
+    }
+    else{
+       write(1, "No Error\n", 9);
+       
+       //Memory for name
+        data->station = (char*) malloc(sizeof(char));
+        count++;
+        read(fd, &ptr, 1);
+        while(ptr != '\n'){
+            data->station[count-1] = ptr;
+            data->station = realloc(data->station, (++count)*sizeof(char));
+            read(fd, &ptr, 1);
+        }
+        data->station[count - 2] = '\0';
+
+        //Memory for path
+        count = 0;
+        data->path = (char*) malloc(sizeof(char));
+        count++;
+        read(fd, &ptr, 1);
+        while(ptr != '\n'){
+            data->path[count-1] = ptr;
+            data->path = realloc(data->path, (++count)*sizeof(char));
+            read(fd, &ptr, 1);
+        }
+        data->path[count - 2] = '\0';
+
+        //Getting time
+        count = 0;
+        read(fd, &ptr, 1);
+        while(ptr != '\n'){
+            buffer[count++] = ptr;
+            read(fd, &ptr, 1);
+        }
+        buffer[count] = '\0';
+        data->time = atoi(buffer);
+        
+        //Memory for Jack server IP
+        count = 0;
+        data->jackIP = (char*) malloc(sizeof(char));
+        count++;
+        read(fd, &ptr, 1);
+        while(ptr != '\n'){
+            data->jackIP[count-1] = ptr;
+            data->jackIP = realloc(data->jackIP, (++count)*sizeof(char));
+            read(fd, &ptr, 1);
+        }
+        data->jackIP[count - 2] = '\0';
+
+        //Memory for Jack port
+        count = 0;
+        data->jackPort = (char*) malloc(sizeof(char));
+        count++;
+        read(fd, &ptr, 1);
+        while(ptr != '\n'){
+            data->jackPort[count-1] = ptr;
+            data->jackPort = realloc(data->jackPort, (++count)*sizeof(char));
+            read(fd, &ptr, 1);
+        }
+        data->jackPort[count - 2] = '\0';
+
+        //Memory for Wendy server IP
+        count = 0;
+        data->wendyIP = (char*) malloc(sizeof(char));
+        count++;
+        read(fd, &ptr, 1);
+        while(ptr != '\n'){
+            data->wendyIP[count-1] = ptr;
+            data->wendyIP = realloc(data->wendyIP, (++count)*sizeof(char));
+            read(fd, &ptr, 1);
+        }
+        data->wendyIP[count - 2] = '\0';
+
+        //Memory for Wendy port
+        count = 0;
+        data->wendyPort = (char*) malloc(sizeof(char));
+        count++;
+        read(fd, &ptr, 1);
+        while(ptr != '\n'){
+            data->wendyPort[count-1] = ptr;
+            data->wendyPort = realloc(data->wendyPort, (++count)*sizeof(char));
+            read(fd, &ptr, 1);
+        }
+        data->wendyPort[count - 2] = '\0';
+
+
+        //Close file
+        close(fd);
+
+        sprintf(buffer, "Station name is -%s-\n", data->station);
+        write(1, buffer, strlen(buffer));
+        sprintf(buffer, "Path name is -%s-\n", data->path);
+        write(1, buffer, strlen(buffer));
+        sprintf(buffer, "Time is -%d-\n", data->time);
+        write(1, buffer, strlen(buffer));
+        sprintf(buffer, "JackIP is -%s-\n", data->jackIP);
+        write(1, buffer, strlen(buffer));
+        sprintf(buffer, "JackPort is -%s-\n", data->jackPort);
+        write(1, buffer, strlen(buffer));
+        sprintf(buffer, "WendyIP is -%s-\n", data->wendyIP);
+        write(1, buffer, strlen(buffer));
+        sprintf(buffer, "WendyPort is -%s-\n", data->wendyPort);
+        write(1, buffer, strlen(buffer));
+    }
+}
 
 void ksighandler(int signum){
     switch (signum)
@@ -55,64 +171,25 @@ void ksighandler(int signum){
     signal(signum, ksighandler);
 }
 
-
 int main(int argc, char const *argv[])
 {
-    char buffer[128];
-    char ptr;
-
-    //Data file processing
     Data data;
-    int fd = -1;
-    int count = 0;
 
     //Reprogram signals
     for (size_t i = 0; i < 64; i++) signal(i, ksighandler);
 
     //Process configuration file
-    sprintf(buffer, "Opening file -%s-\n", argv[argc - 1]);
-    write(1, buffer, strlen(buffer));
-
     write(1, EOL, sizeof(EOL));
     write(1, STARTING, sizeof(STARTING));
     write(1, EOL, sizeof(EOL));
-
-    fd = open(argv[argc-1], O_RDONLY);
-    if (fd < 0) {
-      sprintf(buffer, "Error opening %s", argv[argc-1]);
-      perror(buffer);
-      raise(SIGINT);
-    }
-    else{
-       write(1, "No Error\n", 9);
-       
-       //Memory for name
-        data.station = (char*) malloc(sizeof(char));
-        count++;
-        read(fd, &ptr, 1);
-        while(ptr != '\n'){
-            data.station[count-1] = ptr;
-            data.station = realloc(data.station, (++count)*sizeof(char));
-            read(fd, &ptr, 1);
-        }
-        data.station[count - 2] = '\0';
-
-        //Memory for 
+    processConfig(&data, argv[argc -1]);
 
 
-        sprintf(buffer, "Station name is -%s-\n", data.station);
-        write(1, buffer, strlen(buffer));
-        
-
-    }
-
-
-
-
-    while (1)
+    /*while (1)
     {
         //Listen to folder
-    }
+        pause();
+    }*/
 
 
     return 0;
